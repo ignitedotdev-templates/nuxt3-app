e4a7b624f75d61b21b17408ec3612576df63e956
CharlesChinedum <charleschinedum2@gmail.com>
2024-02-16 12:33:15 +0100

    initial commit

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Welcome To Ignite.dev Nuxt 3 App</h1>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: #3b8070;
+  text-align: center;
+  margin-top: 40px;
+  font-size: 40px;
+  font-weight: 300;
+  font-family: "Roboto", sans-serif;
+  letter-spacing: 1px;
+  text-transform: uppercase;
+  text-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-transition: all 0.3s ease;
+  transition: all 0.3s ease;
+}
+</style>
